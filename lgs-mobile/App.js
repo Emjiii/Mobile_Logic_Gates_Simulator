f12c21d9b4949f6033b52e06920c6f9ce40bc510
MARC JUAREN GAMILLA <124160084+Emjiii@users.noreply.github.com>
2024-12-05 03:06:58 +0800

    With working webview

--- lgs-mobile/App.js
+++ lgs-mobile/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import WebViewWrapper from '../../components/webApp'; // Import your component
+import WebViewWrapper from './src/WebViewWrapper'; // Import your component
 
 export default function App() {
   return <WebViewWrapper />;
